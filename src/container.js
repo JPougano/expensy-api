15182cf6b256b86329a6596da4b6ec99386c38de
JPougano <pedroougano@gmail.com>
2023-10-01 19:55:22 -0300

    feat: :fire: add logger function to container

--- src/container.js
+++ src/container.js
@@ -1,11 +1,14 @@
 const { asFunction, createContainer } = require("awilix");
 const server = require("./application/server");
 const router = require("./application/router");
+const logger = require("./infra/logger")
+
 const container = createContainer();
 
 container.register({
     server: asFunction(server).singleton(),
     router: asFunction(router).singleton(),
+    logger: asFunction(logger).singleton(),
   });
   
 const serverInstance = container.resolve('server');
