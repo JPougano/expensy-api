d58a06eecd344fc10286573762fd47364cf271fe
JPougano <pedroougano@gmail.com>
2023-10-01 19:53:25 -0300

    feat: :fire: make server and exportable dependency

---
+++ src/application/server.js
@@ -0,0 +1,25 @@
+const Koa = require("koa");
+const util = require('util');
+const { PORT } = process.env;
+
+module.exports = ({ router, logger }) => {
+  const app = new Koa();
+
+  app.use(router.routes());
+
+  return {
+    app,
+    start: async () => {
+      try {
+        app.listen(PORT, () => {
+          logger.info(`Server listening on ${PORT}`);
+        });
+      } catch (err) {
+        logger.error("Problem initializing server", {
+          error: util.inspect(err),
+        });
+        process.exit(1);
+      }
+    },
+  };
+};
