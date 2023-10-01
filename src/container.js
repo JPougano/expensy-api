9d9fa55e869653fda19e5af3dd78e98d4de9bfff
JPougano <pedroougano@gmail.com>
2023-10-01 20:33:16 -0300

    feat: :fire: add maintenance controller to container

--- src/container.js
+++ src/container.js
@@ -2,14 +2,18 @@ const { asFunction, createContainer } = require("awilix");
 const server = require("./application/server");
 const router = require("./application/router");
 const logger = require("./infra/logger")
+const maintenanceController = require("./application/controller/maintenance")
 
 const container = createContainer();
 
 container.register({
-    server: asFunction(server).singleton(),
-    router: asFunction(router).singleton(),
-    logger: asFunction(logger).singleton(),
-  });
+  server: asFunction(server).singleton(),
+  router: asFunction(router).singleton(),
+  logger: asFunction(logger).singleton(),
+
+  //Controllers
+  maintenanceController: asFunction(maintenanceController).scoped(),
+});
   
 const serverInstance = container.resolve('server');
 serverInstance.start();
