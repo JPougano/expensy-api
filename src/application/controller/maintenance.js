8af5d9a90bd395c159749ecf24a7a5b7484f1896
JPougano <pedroougano@gmail.com>
2023-10-01 22:05:35 -0300

    feat: :fire: create readiness endpoint

--- src/application/controller/maintenance.js
+++ src/application/controller/maintenance.js
@@ -1,10 +1,16 @@
-const { OK } = require("http-status");
+const { OK, INTERNAL_SERVER_ERROR } = require("http-status");
 
-module.exports = () => {
+module.exports = ({ mongoHelper }) => {
   const healthCheck = async (ctx) => {
     ctx.status = OK;
     ctx.body = "OK";
   };
 
-  return { healthCheck };
+  const readiness = async (ctx) => {
+    const applicationIsReady = mongoHelper.isMongoAlive() === 1;
+    ctx.status = applicationIsReady ? OK : INTERNAL_SERVER_ERROR;
+    ctx.body = applicationIsReady ? "OK" : "NOT-OK";
+  };
+
+  return { healthCheck, readiness };
 };
--- src/application/router.js
+++ src/application/router.js
@@ -4,5 +4,6 @@ module.exports = ({ maintenanceController }) => {
   const router = new Router();
 
   router.get("/healthcheck", maintenanceController.healthCheck);
+  router.get("/readiness", maintenanceController.readiness);
   return router;
 };
