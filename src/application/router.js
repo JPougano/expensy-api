2e0c8c439221dae946c5acb9911747a3a49b0471
JPougano <pedroougano@gmail.com>
2023-10-02 15:08:41 -0300

    feat: :fire: add create user route

--- src/application/router.js
+++ src/application/router.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router');
 
-module.exports = ({ maintenanceController }) => {
+module.exports = ({ maintenanceController, userController }) => {
   const router = new Router();
 
   router.get("/healthcheck", maintenanceController.healthCheck);
   router.get("/readiness", maintenanceController.readiness);
+  router.post("/user", userController.create);
   return router;
 };
