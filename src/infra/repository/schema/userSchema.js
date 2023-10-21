d1673f41c07fb4f93a05234d1137e309c18999a1
JPougano <pedroougano@gmail.com>
2023-10-21 00:04:23 -0300

    feat: :construction: update userSchema

--- src/infra/repository/schema/userSchema.js
+++ src/infra/repository/schema/userSchema.js
@@ -1,9 +1,28 @@
 module.exports = ({ mongoose }) => {
   const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    created_at: { type: Date, required: true, default: Date.now },
+    username: {
+      type: String,
+      min: 3,
+      max: 255,
+      required: true,
+    },
+    email: {
+      type: String,
+      min: 6,
+      max: 255,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      min: 6,
+      max: 1024,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
   });
 
   const UserModel = mongoose.model("User", userSchema);
