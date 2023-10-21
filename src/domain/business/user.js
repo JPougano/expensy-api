c1bcaac2dedcdb26e099b70f6d28424d2ccff943
JPougano <pedroougano@gmail.com>
2023-10-21 00:31:22 -0300

    feat: :sparkles: use userValidation in business

--- src/domain/business/user.js
+++ src/domain/business/user.js
@@ -1,18 +1,21 @@
-module.exports =({logger, userRepository}) => {
-    const createUser = async (userData) => {
-        try {
-          const createdUser = await userRepository.add(userData);
-          logger.info(`New User Created. id: ${createdUser.id}`);
-          return createdUser;
-        } catch (error) {
-          logger.error(`Error trying to save new user`, {
-            error
-          });
-          throw error;
-        }
-      };
-    
-      return {
-        createUser,
-      };
-}
+const Joi = require("joi");
+
+module.exports = ({ logger, userRepository, userValidation }) => {
+  const createUser = async (userData) => {
+    try {
+      Joi.validate(userData, userValidation.createUserModel);
+      const createdUser = await userRepository.add(userData);
+      logger.info(`New User Created. id: ${createdUser.id}`);
+      return createdUser;
+    } catch (error) {
+      logger.error(`Error trying to save new user`, {
+        error,
+      });
+      throw error;
+    }
+  };
+
+  return {
+    createUser,
+  };
+};
