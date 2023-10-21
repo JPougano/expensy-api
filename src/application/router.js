40ccd06d142b4e54135fda03aa286211926f458b
JPougano <pedroougano@gmail.com>
2023-10-21 00:03:48 -0300

    refactor: :construction:

--- src/application/router.js
+++ src/application/router.js
@@ -3,8 +3,9 @@ const Router = require('koa-router');
 module.exports = ({ maintenanceController, userController }) => {
   const router = new Router();
 
-  router.get("/healthcheck", maintenanceController.healthCheck);
-  router.get("/readiness", maintenanceController.readiness);
-  router.post("/user", userController.create);
+  router.get("/api/healthcheck", maintenanceController.healthCheck);
+  router.get("/api/readiness", maintenanceController.readiness);
+  router.post("/api/user/register", userController.create);
+
   return router;
 };
