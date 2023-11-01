be1ad9dd91069e877cd9dc5763f5d7c1f94f49e1
João Ougano <pedroougano@gmail.com>
2023-11-01 00:32:16 -0300

    feat: :sparkles: add initial auth routing

--- src/application/router.js
+++ src/application/router.js
@@ -1,11 +1,16 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 
-module.exports = ({ maintenanceController, userController }) => {
+module.exports = ({ maintenanceController, userController, authController, authMiddleware }) => {
   const router = new Router();
 
   router.get("/api/healthcheck", maintenanceController.healthCheck);
   router.get("/api/readiness", maintenanceController.readiness);
   router.post("/api/user/register", userController.create);
+  router.post("/api/auth/register", authController.register);
+  router.post("/api/user/login", authController.login);
+  router.get("/profile", authMiddleware.authenticate, (ctx) => {
+    ctx.body({ message: `Welcome ${req.user.username}` });
+  });
 
   return router;
 };
