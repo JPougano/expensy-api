63664fc381be2a68411f994127907373c38aee73
João Ougano <pedroougano@gmail.com>
2023-11-01 00:29:28 -0300

    refactor: :recycle:

--- src/infra/repository/schema/userSchema.js
+++ src/infra/repository/schema/userSchema.js
@@ -1,30 +1,49 @@
-module.exports = ({ mongoose }) => {
-  const userSchema = mongoose.Schema({
-    username: {
-      type: String,
-      min: 3,
-      max: 255,
-      required: true,
-    },
-    email: {
-      type: String,
-      min: 6,
-      max: 255,
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      min: 6,
-      max: 1024,
-      required: true,
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-  });
+const bcrypt = require('bcrypt');
 
-  const UserModel = mongoose.model("User", userSchema);
-  return UserModel;
+module.exports = ({ mongoose, logger }) => {
+  const userSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        required: [true, "username is required"],
+        unique: true
+      },
+      email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true
+      },
+      password: {
+        type: String,
+        required: [true, "password is required"]
+      },
+    },
+    { timestamps: true }
+  );
+  
+  userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+  
+    try {
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(user.password, salt);
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  });
+  
+  userSchema.methods.comparePassword = async function (password) {
+    logger.info(JSON.stringify({
+      password,
+      thisPass: this.password
+    }))
+    return bcrypt.compare(password, this.password);
+  };
+  
+  const User = mongoose.model('User', userSchema);
+  
+  
+  return User;
 };
