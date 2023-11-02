b0b678a4ca1fb4a2d4f87b94add69c1760db26e6
João Ougano <pedroougano@gmail.com>
2023-11-02 00:55:37 -0300

    refactor: :zap: update userController from authController

--- src/application/controller/user.js
+++ src/application/controller/user.js
@@ -1,27 +1,66 @@
-const { CREATED } = require("http-status");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const httpStatus = require("http-status");
 
-const defaultHeaders = {
-  "Content-Type": "application/json",
-  "Cache-Control": "no-store",
-};
-
-module.exports = ({ logger, userBusiness }) => {
+module.exports = ({ userBusiness, ClientErrors, logger }) => {
   const create = async (ctx) => {
+    const { username, email, password } = ctx.request.body;
+
     try {
-      const user = await userBusiness.createUser(ctx.request.body);
-      ctx.response.status = CREATED;
-      ctx.response.headers = defaultHeaders;
-      ctx.response.body = user;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await userBusiness.createUser({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      ctx.response.status = httpStatus.CREATED;
+      ctx.response.body = { message: "User successfully ", user };
     } catch (error) {
-      if (error.name === "ClientError") {
-        ctx.throw(error);
+      if (error.code) {
+        logger.error("Error creating user", error);
+        ctx.throw(ClientErrors.AlreadyExistsError);
       }
+
       logger.error("Error creating user", error);
-      ctx.throw(new Error("There was an unexpected error creating user"));
+      ctx.throw(httpStatus.INTERNAL_SERVER_ERROR);
     }
   };
 
-  return {
-    create,
+  const login = async (ctx) => {
+    const { email, password } = ctx.request.body;
+
+    try {
+      const user = await userBusiness.getUser(email);
+
+      if (!user) {
+        throw ClientErrors.NotFound;
+      }
+      const passwordMatch = await user.comparePassword(password);
+
+      if (!passwordMatch) {
+        throw ClientErrors.InvalidCredentials;
+      }
+
+      const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+        expiresIn: "1 hour",
+      });
+
+      ctx.response.status = httpStatus.OK;
+      ctx.response.body = { token };
+    } catch (error) {
+      logger.error("Failed to login:", error);
+
+      if (error.status === httpStatus.UNAUTHORIZED) {
+        ctx.throw(ClientErrors.InvalidCredentials);
+      }
+
+      if (error.status === httpStatus.NOT_FOUND) {
+        ctx.throw(ClientErrors.NotFound);
+      }
+
+      ctx.throw(httpStatus.INTERNAL_SERVER_ERROR);
+    }
   };
+  return { create, login };
 };
